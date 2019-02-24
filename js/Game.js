3ba018fcfa5db8c51e0bb43f95740d497f481b8f
SeyedMortezaKamali <smkplus3d@gmail.com>
2019-02-24 21:57:29 +0330

    Test

--- js/Game.js
+++ js/Game.js
@@ -10,29 +10,29 @@ var Game = function () {
 
     // First row
     
-    new Ball(X_offset, Ball.RADIUS, 4 * Ball.RADIUS, '4ball'),
-    new Ball(X_offset, Ball.RADIUS, 2 * Ball.RADIUS, '3ball'),
-    new Ball(X_offset, Ball.RADIUS, 0, '14ball'),
-    new Ball(X_offset, Ball.RADIUS, -2 * Ball.RADIUS, '2ball'),
-    new Ball(X_offset, Ball.RADIUS, -4 * Ball.RADIUS, '15ball'),
+    new Ball(X_offset, Ball.RADIUS, 4 * Ball.RADIUS, 'whiteball'),
+    new Ball(X_offset, Ball.RADIUS, 2 * Ball.RADIUS, 'whiteball'),
+    new Ball(X_offset, Ball.RADIUS, 0, 'whiteball'),
+    new Ball(X_offset, Ball.RADIUS, -2 * Ball.RADIUS, 'whiteball'),
+    new Ball(X_offset, Ball.RADIUS, -4 * Ball.RADIUS, 'whiteball'),
 
     // 2nd row
-    new Ball(X_offset - X_offset_2 * Ball.RADIUS, Ball.RADIUS, 3 * Ball.RADIUS, '13ball'),
-    new Ball(X_offset - X_offset_2 * Ball.RADIUS, Ball.RADIUS, Ball.RADIUS, '7ball'),
-    new Ball(X_offset - X_offset_2 * Ball.RADIUS, Ball.RADIUS, -1 * Ball.RADIUS, '12ball'),
-    new Ball(X_offset - X_offset_2 * Ball.RADIUS, Ball.RADIUS, -3 * Ball.RADIUS, '5ball'),
+    new Ball(X_offset - X_offset_2 * Ball.RADIUS, Ball.RADIUS, 3 * Ball.RADIUS, 'whiteball'),
+    new Ball(X_offset - X_offset_2 * Ball.RADIUS, Ball.RADIUS, Ball.RADIUS, 'whiteball'),
+    new Ball(X_offset - X_offset_2 * Ball.RADIUS, Ball.RADIUS, -1 * Ball.RADIUS, 'whiteball'),
+    new Ball(X_offset - X_offset_2 * Ball.RADIUS, Ball.RADIUS, -3 * Ball.RADIUS, 'whiteball'),
 
     // 3rd row
-    new Ball(X_offset - X_offset_2 * 2 * Ball.RADIUS, Ball.RADIUS, 2 * Ball.RADIUS, '6ball'),
-    new Ball(X_offset - X_offset_2 * 2 * Ball.RADIUS, Ball.RADIUS, 0, '8ball'),
-    new Ball(X_offset - X_offset_2 * 2 * Ball.RADIUS, Ball.RADIUS, -2 * Ball.RADIUS, '9ball'),
+    new Ball(X_offset - X_offset_2 * 2 * Ball.RADIUS, Ball.RADIUS, 2 * Ball.RADIUS, 'whiteball'),
+    new Ball(X_offset - X_offset_2 * 2 * Ball.RADIUS, Ball.RADIUS, 0, 'whiteball'),
+    new Ball(X_offset - X_offset_2 * 2 * Ball.RADIUS, Ball.RADIUS, -2 * Ball.RADIUS, 'whiteball'),
 
     //4th row
-    new Ball(X_offset - X_offset_2 * 3 * Ball.RADIUS, Ball.RADIUS, Ball.RADIUS, '10ball'),
-    new Ball(X_offset - X_offset_2 * 3 * Ball.RADIUS, Ball.RADIUS, -1 * Ball.RADIUS, '11ball'),
+    new Ball(X_offset - X_offset_2 * 3 * Ball.RADIUS, Ball.RADIUS, Ball.RADIUS, 'whiteball'),
+    new Ball(X_offset - X_offset_2 * 3 * Ball.RADIUS, Ball.RADIUS, -1 * Ball.RADIUS, 'whiteball'),
 
     //5th row
-    new Ball(X_offset - X_offset_2 * 4 * Ball.RADIUS, Ball.RADIUS, 0, '1ball')
+    new Ball(X_offset - X_offset_2 * 4 * Ball.RADIUS, Ball.RADIUS, 0, '1bawhiteballll')
   ];
 }
 
